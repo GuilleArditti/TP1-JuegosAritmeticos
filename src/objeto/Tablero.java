aa9736db3b80780822c3ff514a0697cc58c94619
Marcelo Palacios <jpdjcp@gmail.com>
2023-03-23 20:17:16 -0300

    Tablero class, implemented method: setCelda(), aux methods validarFila() & validarColumna().

--- src/objeto/Tablero.java
+++ src/objeto/Tablero.java
@@ -15,16 +15,20 @@ public class Tablero {
 		generarResultadosColumnas();
 	}
 	
-	public Tablero(int tamanio) {
-		this.cuadricula = new int[tamanio][tamanio];
-		this.resultadosFilas = new int[tamanio];
-		this.resultadosColumnas = new int[tamanio];
+	public Tablero(int dimension) {
+		this.dimension = dimension;
+		this.cuadricula = new int[dimension][dimension];
+		this.resultadosFilas = new int[dimension];
+		this.resultadosColumnas = new int[dimension];
 		generarResultadosFilas();
 		generarResultadosColumnas();
 	}
 	
 	public void setCelda(int num, int fila, int col) {
+		validarFila(fila);
+		validarColumna(col);
 		
+		this.cuadricula[fila][col] = num;
 	}
 	
 	public boolean sumasCorrectas() {
@@ -37,6 +41,20 @@ public class Tablero {
 	
 	private void generarResultadosColumnas() {
 		
+	}
+	
+	private void validarFila(int fila) {
+		if (fila < 0)
+			throw new IllegalArgumentException("La fila no puede ser menor que 0 (cero)");
+		if (fila >= cuadricula.length)
+			throw new IllegalArgumentException("La fila no puede ser mayor o igual que: " + this.dimension);
+	}
+	
+	private void validarColumna(int col) {
+		if (col < 0)
+			throw new IllegalArgumentException("La columna no puede ser menor que 0 (cero)");
+		if (col >= 0)
+			throw new IllegalArgumentException("La columna no puede ser mayor que: " + this.dimension);
 	}
 
 }
