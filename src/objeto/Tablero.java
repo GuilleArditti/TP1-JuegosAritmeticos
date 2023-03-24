da1727e009bb64b9b068564884ce82b99e9729d8
Marcelo Palacios <jpdjcp@gmail.com>
2023-03-23 21:36:22 -0300

    Tablero class: add maximoPosible : int & minimoPosible : int variables. Implemented method: generarResultadosFilas().

--- src/objeto/Tablero.java
+++ src/objeto/Tablero.java
@@ -1,21 +1,28 @@
 package objeto;
 
+import java.util.Random;
+
 public class Tablero {
 
 	private int dimension;
 	private int[] resultadosFilas;
 	private int[] resultadosColumnas;
 	private int[][] cuadricula;
+	private int maximoPosible;
+	private int minimoPosible;
 
 	public Tablero() {
 		this.cuadricula = new int[4][4];
 		this.resultadosFilas = new int[4];
 		this.resultadosColumnas = new int[4];
+		this.maximoPosible = 9 * this.dimension;
+		this.minimoPosible = dimension;
 		generarResultadosFilas();
 		generarResultadosColumnas();
 	}
 	
 	public Tablero(int dimension) {
+		validarDimension(dimension);
 		this.dimension = dimension;
 		this.cuadricula = new int[dimension][dimension];
 		this.resultadosFilas = new int[dimension];
@@ -31,16 +38,23 @@ public class Tablero {
 		this.cuadricula[fila][col] = num;
 	}
 	
+	// TODO
 	public boolean sumasCorrectas() {
 		throw new RuntimeException("Método no implementado");
 	}
 	
+	/** random [(dimension), (9*dimension)] */
 	private void generarResultadosFilas() {
+		Random random = new Random();
 		
+		for (int i = 0; i < resultadosFilas.length; i++) {
+			resultadosFilas[i] = this.minimoPosible + Math.abs(random.nextInt(this.maximoPosible-3));
+		}
 	}
 	
+	// TODO
 	private void generarResultadosColumnas() {
-		
+		throw new RuntimeException("Método no implementado");
 	}
 	
 	private void validarFila(int fila) {
@@ -55,6 +69,13 @@ public class Tablero {
 			throw new IllegalArgumentException("La columna no puede ser menor que 0 (cero)");
 		if (col >= 0)
 			throw new IllegalArgumentException("La columna no puede ser mayor que: " + this.dimension);
+	}
+	
+	private void validarDimension(int dimension) {
+		if (dimension < 4)
+			throw new IllegalArgumentException("La dimension no puede ser menor que 4");
+		if (dimension > 10)
+			throw new IllegalArgumentException("La dimension no puede ser mayor que 10");
 	}
 
 }
